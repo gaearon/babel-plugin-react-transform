3ab32f9d85ad29cd6d103eaf8a5e029238897ccc
Eric Clemmons <eric@smarterspam.com>
2016-02-16 20:00:46 -0600

    Add test for ignoring nested functions

---
+++ test/fixtures/code-functional-ignore-nested-function-declaration/expected.js
@@ -0,0 +1,36 @@
+import _transformLib from "transform-lib";
+const _components = {
+  App: {
+    displayName: "App"
+  }
+};
+
+const _transformLib2 = _transformLib({
+  filename: "/Users/Eric/Projects/ericclemmons/babel-plugin-react-transform/test/fixtures/code-functional-ignore-nested-function-declaration/actual.js",
+  components: _components,
+  locals: [],
+  imports: []
+});
+
+function _wrapComponent(id) {
+  return function (Component) {
+    return _transformLib2(Component, id);
+  };
+}
+
+const App = _wrapComponent("App")(class App extends React.Component {
+  render() {
+    function Component() {
+      return React.createElement(
+        "div",
+        null,
+        "Ignored."
+      );
+    }
+
+    return React.createElement(Component, null);
+  }
+
+});
+
+export default App;
