b9d30fc12668ced612be182459324bae357382ac
Nuno Campos <nuno@crowdprocess.com>
2016-02-18 22:20:25 +0000

    added Component to default superClasses; added test for it

--- src/index.js
+++ src/index.js
@@ -171,7 +171,7 @@ export default function({ types: t, template }) {
     normalizeOptions(options) {
       return {
         factoryMethods: options.factoryMethods || ['React.createClass'],
-        superClasses: options.superClasses || ['React.Component'],
+        superClasses: options.superClasses || ['React.Component', 'Component'],
         transforms: options.transforms.map(opts => {
           return {
             transform: opts.transform,
